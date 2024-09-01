70738cced861dbd245621c1c5585a176e0f9bb6d
Marvin Céspedes <mcespo@gmail.com>
2024-08-31 23:04:55 -0500

    prettier and remove lightbox, footer nav

--- src/libs/swiper.js
+++ src/libs/swiper.js
@@ -1,20 +1,20 @@
-import Swiper from "swiper";
-import { Navigation, Autoplay } from "swiper/modules";
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/thumbs";
+import Swiper from 'swiper';
+import { Navigation, Autoplay } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/thumbs';
 
-document.addEventListener("DOMContentLoaded", function () {
-  const main_swiper = new Swiper(".main", {
+document.addEventListener('DOMContentLoaded', function () {
+  const main_swiper = new Swiper('.main', {
     modules: [Navigation, Autoplay],
     centeredSlides: true,
     autoplay: {
-        delay: 5000,
+      delay: 5000,
     },
     spaceBetween: 10,
     navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    }
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
   });
 });
